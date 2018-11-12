7cff81532e6c7d10363b4ffb66df7cfa6857ef77
Tailor <tailor@toshyro.net>
2018-11-12 20:35:30 -0200

    Primeiro commit.

---
+++ juris.net/Controllers/ClienteController.cs
@@ -0,0 +1,23 @@
+﻿using Juris.net.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Juris.net.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View(new List<ClienteViewModel>());
+        }
+
+        public ActionResult Add()
+        {
+            return View(new ClienteViewModel());
+        }
+    }
+}
